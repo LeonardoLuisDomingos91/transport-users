ad2540b2046e1a9e83ef683fa5c5b1e078b86e64
Leonardo Luis Domingos <you@example.com>
2020-12-04 21:18:19 -0300

    subindo o projeto cru

---
+++ src/test/java/br/com/invillia/lyon/userevents/UserEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.invillia.lyon.userevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
